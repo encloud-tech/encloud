af1b810308c002f655530d37718a749e635584f7
Vivek Shah <vivekkshah7@gmail.com>
2022-09-13 19:10:31 +0530

    #4: Add dynamic values in test case.

--- constants/constant.go
+++ constants/constant.go
@@ -2,5 +2,5 @@ package constants
 
 const EstuaryApiShuttleBaseUrl = "https://shuttle-4.estuary.tech"
 const EstuaryApiBaseUrl = "https://api.estuary.tech"
-const EstuaryApiToken = "EST63c0883e-089b-4db5-9d08-80d568fd86a8ARY"
+const EstuaryApiToken = "ESTb2e5e305-1af1-4c72-89ab-c85404439fcdARY"
 const EstuaryDownloadApiUrl = "https://dweb.link/ipfs"
