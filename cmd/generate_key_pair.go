0c806c15ca7add20de759b74af369a3b72f28ce0
Vivek Shah <vivekkshah7@gmail.com>
2022-09-30 17:56:36 +0530

    #8: Add cmd test case.

--- cmd/generate_key_pair.go
+++ cmd/generate_key_pair.go
@@ -11,29 +11,33 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var generateKeyPairCmd = &cobra.Command{
-	Use:   "generate-key-pair",
-	Short: "Generate your key pair",
-	Long:  `Generate your public key and private key which helps to encrypt and decrypt your data`,
-	Run: func(cmd *cobra.Command, args []string) {
-		thirdparty.InitCrypto()
-		response := types.GenerateKeyPairResponse{
-			Status:     "success",
-			StatusCode: http.StatusCreated,
-			Message:    "Keys generated successfully.",
-			Data:       types.Keys{PublicKey: thirdparty.GetIdRsaPubStr(), PrivateKey: thirdparty.GetIdRsaStr()},
-		}
-		os.Remove(".keys/.idRsaPub")
-		os.Remove(".keys/.idRsa")
-		encoded, err := json.Marshal(response)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		fmt.Println(string(encoded))
-	},
+func GenerateKeyPairCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "generate-key-pair",
+		Short: "Generate your key pair",
+		Long:  `Generate your public key and private key which helps to encrypt and decrypt your data`,
+		Run: func(cmd *cobra.Command, args []string) {
+			thirdparty.InitCrypto()
+			response := types.GenerateKeyPairResponse{
+				Status:     "success",
+				StatusCode: http.StatusCreated,
+				Message:    "Keys generated successfully.",
+				Data:       types.Keys{PublicKey: thirdparty.GetIdRsaPubStr(), PrivateKey: thirdparty.GetIdRsaStr()},
+			}
+			os.Remove(".keys/.idRsaPub")
+			os.Remove(".keys/.idRsa")
+			encoded, err := json.Marshal(response)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			fmt.Fprintf(cmd.OutOrStdout(), string(encoded))
+		},
+	}
+
+	return cmd
 }
 
 func init() {
-	rootCmd.AddCommand(generateKeyPairCmd)
+	RootCmd.AddCommand(GenerateKeyPairCmd())
 }
--- cmd/list.go
+++ cmd/list.go
@@ -11,31 +11,35 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var listContentsCmd = &cobra.Command{
-	Use:   "list",
-	Short: "List your uploaded contents",
-	Long:  `List your uploaded contents data which contains file meta informations`,
-	Run: func(cmd *cobra.Command, args []string) {
-		kek, _ := cmd.Flags().GetString("publicKey")
-		fileMetaData := service.Fetch(kek)
-		os.Remove("assets/downloaded.bin")
-		response := types.ListContentResponse{
-			Status:     "success",
-			StatusCode: http.StatusFound,
-			Message:    "Content fetched successfully.",
-			Data:       fileMetaData,
-		}
-		encoded, err := json.Marshal(response)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		fmt.Println(string(encoded))
-	},
+func ListContentsCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "list",
+		Short: "List your uploaded contents",
+		Long:  `List your uploaded contents data which contains file meta informations`,
+		Run: func(cmd *cobra.Command, args []string) {
+			kek, _ := cmd.Flags().GetString("publicKey")
+			fileMetaData := service.Fetch(kek)
+			os.Remove("assets/downloaded.bin")
+			response := types.ListContentResponse{
+				Status:     "success",
+				StatusCode: http.StatusFound,
+				Message:    "Content fetched successfully.",
+				Data:       fileMetaData,
+			}
+			encoded, err := json.Marshal(response)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			fmt.Fprintf(cmd.OutOrStdout(), string(encoded))
+		},
+	}
+
+	cmd.Flags().StringP("publicKey", "p", "", "Enter your public key")
+	cmd.MarkFlagRequired("publicKey")
+	return cmd
 }
 
 func init() {
-	rootCmd.AddCommand(listContentsCmd)
-	listContentsCmd.Flags().StringP("publicKey", "p", "", "Enter your public key")
-	listContentsCmd.MarkFlagRequired("publicKey")
+	RootCmd.AddCommand(ListContentsCmd())
 }
